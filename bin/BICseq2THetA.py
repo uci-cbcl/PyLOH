8713a456239bfb92bc35696cb39d7bfc37edf46a
Yi Li <yil8@uci.edu>
2013-07-19 13:50:46 -0700

    commite before add user defined segmentation

--- bin/BICseq2THetA.py
+++ bin/BICseq2THetA.py
@@ -20,15 +20,16 @@ def main():
             continue
         
         chrom, start, end, tumorCount, normalCount, __, __ = line.strip('\n').split('\t')
-        chrom_len = int(end) - int(start)
+        segment_len = int(end) - int(start) + 1
         
-        if chrom_len < len_thred or chrom not in CHROM_LIST:
+        if segment_len < len_thred or chrom not in CHROM_LIST:
             continue
         
         chrom = chrom.strip('chr')
         
         ID = 'start_%s_%s:end_%s_%s' % (chrom, start, chrom, end)
 
+        print('\t'.join([ID, chrom, start, end, tumorCount, normalCount, str(segment_len)]))
         outfile.write('\t'.join([ID, chrom, start, end, tumorCount, normalCount]) + '\n')
         
         
