ae5030eff861f598dec0ece59b56a5a73edee54d
Yi Li <yil8@uci.edu>
2013-08-07 23:06:37 -0700

    BICseq2THetA.py len_thred change to userdefined

--- bin/BICseq2THetA.py
+++ bin/BICseq2THetA.py
@@ -8,11 +8,12 @@ CHROM_LIST = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8',
               'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
               'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22']
 
-len_thred = 500000
+#len_thred = 50000
 
 def main():
     infile = open(sys.argv[1])
     outfile = open(sys.argv[2], 'w')
+    len_thred = int(sys.argv[3])
     
     outfile.write('\t'.join(['#ID', 'chrm', 'start', 'end', 'tumorCount', 'normalCount']) + '\n')
     for line in infile:
