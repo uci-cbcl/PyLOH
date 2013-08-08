3fc3e11ca75be4db73082231bc1d80f5b68385af
Yi Li <yil8@uci.edu>
2013-08-07 23:00:06 -0700

    BICseq2THetA.py 50000 len_thred change to 500000

--- bin/BICseq2THetA.py
+++ bin/BICseq2THetA.py
@@ -8,7 +8,7 @@ CHROM_LIST = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8',
               'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
               'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22']
 
-len_thred = 50000
+len_thred = 500000
 
 def main():
     infile = open(sys.argv[1])
