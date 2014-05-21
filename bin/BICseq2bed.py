bfa86d365c3ef0d363bbfe1eec0dc070c37caa0c
Yi Li <yil8@uci.edu>
2014-05-21 14:32:31 -0700

    Update BICseq2bed.py
    ENSEMBL annotation added for chroms

--- bin/BICseq2bed.py
+++ bin/BICseq2bed.py
@@ -14,7 +14,9 @@ import argparse
 
 CHROM_LIST = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8',
               'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
-              'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22']
+              'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22',
+              '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+              '13', '14', '15', '16', '17', '18', '19', '20', '21', '22']
 
 def main():
     parser = argparse.ArgumentParser(description='Converting BICseq segments file to BED file')
